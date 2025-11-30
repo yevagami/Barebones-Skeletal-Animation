056667711abb09e842cb478681f0906ba735d43e
yevagami <87428687+yevagami@users.noreply.github.com>
2025-11-30 12:32:14 -0500

    We done

--- Skeletal-Animation.cpp
+++ Skeletal-Animation.cpp
@@ -150,67 +150,6 @@ inline SDL_Window* initWindow(int& windowWidth, int& windowHeight) {
 
 #pragma endregion
 
-#pragma region SHADERS
-const char* vertexShaderSource = R"(
-	#version 440 core
-	layout (location = 0) in vec3 position; 
-	layout (location = 1) in vec3 normal;
-	layout (location = 2) in vec2 uv;
-	layout (location = 3) in vec4 boneIds;
-	layout (location = 4) in vec4 boneWeights;
-
-	out vec2 tex_cord;
-	out vec3 v_normal;
-	out vec3 v_pos;
-	out vec4 bw;
-
-	uniform mat4 bone_transforms[100];
-	uniform mat4 view_projection_matrix;
-	uniform mat4 model_matrix;
-
-	void main()
-	{
-		bw = vec4(0);
-		if(int(boneIds.x) == 1)
-		bw.z = boneIds.x;
-		//boneWeights = normalize(boneWeights);
-		mat4 boneTransform  =  mat4(0.0);
-		boneTransform  +=    bone_transforms[int(boneIds.x)] * boneWeights.x;
-		boneTransform  +=    bone_transforms[int(boneIds.y)] * boneWeights.y;
-		boneTransform  +=    bone_transforms[int(boneIds.z)] * boneWeights.z;
-		boneTransform  +=    bone_transforms[int(boneIds.w)] * boneWeights.w;
-		vec4 pos = boneTransform * vec4(position, 1.0);
-		gl_Position = view_projection_matrix * model_matrix * pos;
-		v_pos = vec3(model_matrix * boneTransform * pos);
-		tex_cord = uv;
-		v_normal = mat3(transpose(inverse(model_matrix * boneTransform))) * normal;
-		v_normal = normalize(v_normal);
-	}
-
-	)";
-const char* fragmentShaderSource = R"(
-	#version 440 core
-
-	in vec2 tex_cord;
-	in vec3 v_normal;
-	in vec3 v_pos;
-	in vec4 bw;
-	out vec4 color;
-
-	uniform sampler2D diff_texture;
-
-	vec3 lightPos = vec3(0.2, 1.0, -3.0);
-	
-	void main()
-	{
-		vec3 lightDir = normalize(lightPos - v_pos);
-		float diff = max(dot(v_normal, lightDir), 0.2);
-		vec3 dCol = diff * texture(diff_texture, tex_cord).rgb; 
-		color = vec4(dCol, 1);
-	}
-	)";
-#pragma endregion
-
 #pragma region STRUCTS
 //Vertex
 struct Vertex {
@@ -257,6 +196,8 @@ struct Mesh {
 	Bone skeleton; //The root node of the skeleton tree
 	//std::unordered_map<std::string, std::pair<int, glm::mat4>> boneInfoTable;//For debugging purposes, delete later
 	unsigned int vao = 0;
+	unsigned int vbo = 0;
+	unsigned int ebo = 0;
 	unsigned int textureID;
 };
 
@@ -266,6 +207,76 @@ struct Model{
 	std::vector<Animation> animations;
 	glm::mat4 modelMatrix;
 };
+
+struct Shader {
+	unsigned int program;
+	unsigned int vertID;
+	unsigned int fragID;
+};
+
+#pragma endregion
+
+#pragma region SHADERS
+const char* vertexShaderSource = R"(
+	#version 440 core
+	layout (location = 0) in vec3 position; 
+	layout (location = 1) in vec3 normal;
+	layout (location = 2) in vec2 uv;
+	layout (location = 3) in vec4 boneIds;
+	layout (location = 4) in vec4 boneWeights;
+
+	out vec2 tex_cord;
+	out vec3 v_normal;
+	out vec3 v_pos;
+	out vec4 bw;
+
+	uniform mat4 bone_transforms[100];
+	uniform mat4 view_projection_matrix;
+	uniform mat4 model_matrix;
+
+	void main()
+	{
+		bw = vec4(0);
+		if(int(boneIds.x) == 1)
+		bw.z = boneIds.x;
+		//boneWeights = normalize(boneWeights);
+		mat4 boneTransform  =  mat4(0.0);
+		boneTransform  +=    bone_transforms[int(boneIds.x)] * boneWeights.x;
+		boneTransform  +=    bone_transforms[int(boneIds.y)] * boneWeights.y;
+		boneTransform  +=    bone_transforms[int(boneIds.z)] * boneWeights.z;
+		boneTransform  +=    bone_transforms[int(boneIds.w)] * boneWeights.w;
+		vec4 pos = boneTransform * vec4(position, 1.0);
+		gl_Position = view_projection_matrix * model_matrix * pos;
+		v_pos = vec3(model_matrix * boneTransform * pos);
+		tex_cord = uv;
+		v_normal = mat3(transpose(inverse(model_matrix * boneTransform))) * normal;
+		v_normal = normalize(v_normal);
+	}
+
+)";
+
+const char* fragmentShaderSource = R"(
+	#version 440 core
+
+	in vec2 tex_cord;
+	in vec3 v_normal;
+	in vec3 v_pos;
+	in vec4 bw;
+	out vec4 color;
+
+	uniform sampler2D diff_texture;
+
+	vec3 lightPos = vec3(0.2, 1.0, -3.0);
+	
+	void main()
+	{
+		vec3 lightDir = normalize(lightPos - v_pos);
+		float diff = max(dot(v_normal, lightDir), 0.2);
+		vec3 dCol = diff * texture(diff_texture, tex_cord).rgb; 
+		color = vec4(dCol, 1);
+	}
+)";
+
 
 #pragma endregion
 
@@ -840,6 +851,8 @@ int main(int argc, char *argv[]) {
 		//For this demo there is only 1 animation for this mode
 		Animation& currentAnim = dancingRobot.animations[0];
 
+		GUI::Render();
+
 		//Note: 
 		//There is a bug with multiple meshes and 1 skeleton that I haven't figured out a fix yet
 		//I suspect that it has something to do with mismatched bones
@@ -868,10 +881,6 @@ int main(int argc, char *argv[]) {
 			glDrawElements(GL_TRIANGLES, dancingRobot.meshes[i].indices.size(), GL_UNSIGNED_INT, 0);
 		}
 
-
-		GUI::Render();
-
-
 		SDL_GL_SwapWindow(window);
 	}
 
@@ -882,6 +891,15 @@ int main(int argc, char *argv[]) {
 	Mix_FreeMusic(music);
 	music = nullptr;
 	Mix_Quit();
+
+	for (int i = 0; i < dancingRobot.meshes.size(); i++) {
+		glDeleteBuffers(1, &dancingRobot.meshes[i].vbo);
+		glDeleteVertexArrays(1, &dancingRobot.meshes[i].vao);
+		glDeleteBuffers(1, &dancingRobot.meshes[i].ebo);
+	}
+
+	glDeleteTextures(1, &textureLocation);
+	glDeleteProgram(shader);
 
 	SDL_GLContext context =  SDL_GL_GetCurrentContext();
 	SDL_GL_DeleteContext(context);
